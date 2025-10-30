14b66026577d3fcb1fd1be1636a9d92f0034e604
Giovanna Lima de Souza <giovanna.lima1@aluno.ifsp.edu.br>
2025-10-30 20:37:18 -0300

    correções com codigo antigo de app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
-from routes.auth_route import auth_bp
+from datetime import timedelta
+from routes.auth_route import auth_bp, blacklist
 from routes.usuario_route import usuario_bp
 from routes.artista_route import artista_bp
 from routes.empresa_route import empresa_bp
@@ -10,10 +11,15 @@ from routes.vaga_route import vaga_bp
 app = Flask(__name__)
 
 app.config["JWT_SECRET_KEY"] = "seu-segredo-super-seguro"
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = 3600
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
 
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
+
+@jwt.token_in_blocklist_loader
+def check_if_token_in_blacklist(jwt_header, jwt_payload):
+    jti = jwt_payload["jti"]
+    return jti in blacklist
 
 app.register_blueprint(auth_bp, url_prefix="/auth")
 app.register_blueprint(usuario_bp, url_prefix="/usuarios")
